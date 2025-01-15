9c52a7174dfb17ce03b43c013ce73130c25a9133
Luca Mansi <luca@mansi.fr>
2025-01-15 22:57:53 +0100

    Update fizzbuzz and print_comb3 scripts for improved functionality; refactor uppercase function to build result string

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    for j in range(i + 1, 100):
-        if i != j:
-            if i < 99:
-                print("{:02d}, {:02d}".format(i, j), end=", ")
-            else:
-                print("{:02d}, {:02d}".format(i, j))
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:02d}{:02d}".format(i, j))
+        else:
+            print("{:02d}{:02d}".format(i, j), end=", ")
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{:c}".format(ord(str[i]) - 32), end="")
+            result += chr(ord(str[i]) - 32)
         else:
-            print("{:c}".format(ord(str[i])), end="")
-    print()
+            result += str[i]
+    print("{}".format(result))
