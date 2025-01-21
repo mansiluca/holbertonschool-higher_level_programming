01faeb9a4f3e546567e4f0dfb0c047a9a6e0d6a9
Luca Mansi <luca@mansi.fr>
2025-01-21 14:57:24 +0100

    Fix typos in safe_print_list and safe_print_integer functions to improve exception handling

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_lisdt(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
         print()
         return x
-    except:
+    except IndexError:
         print()
         return i
