92bea2c2fe732ad824b37a7e7640262192fa0fad
Luca Mansi <luca@mansi.fr>
2025-02-03 13:30:38 +0100

    Refactor Square and Rectangle classes to import BaseGeometry and streamline area calculations

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,49 +4,7 @@ Class BaseGeometry
 """
 
 
-class BaseGeometry:
-    """
-    Class BaseGeometry
-    """
-
-    def area(self):
-        """
-        Method that raises an Exception with the message
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Method that validates value
-        """
-        if not isinstance(value, int):
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """
-    Class Rectangle
-    """
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Method that returns the area of the rectangle
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Method that returns the rectangle description
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,6 +3,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     Class Rectangle
@@ -11,5 +12,6 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
         self.__width = width
         self.__height = height
