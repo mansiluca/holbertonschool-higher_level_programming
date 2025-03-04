028c6f408b4e542e7bcdf4a26f3b8d136e7477a6
Luca Mansi <luca@mansi.fr>
2025-03-04 15:58:34 +0100

    Refactor SQL scripts to enhance query efficiency and readability

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- dump from hbtn_0d_tvshows to your MySQL server: download (same as 12-no_genre.sql)
-SELECT genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM genres INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id GROUP BY genres.name ORDER BY number_of_shows DESC, genre;
+SELECT genres.name AS genre, COUNT(*) AS number_of_shows FROM genres INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id GROUP BY genres.name HAVING number_of_shows > 0 ORDER BY number_of_shows DESC;
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.name FROM cities WHERE cities.state_id = (SELECT states.id FROM states WHERE states.name = 'California');
+SELECT id, name FROM cities WHERE cities.state_id = (SELECT states.id FROM states WHERE states.name = 'California') ORDER BY id ASC;
