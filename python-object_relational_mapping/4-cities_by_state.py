d202674855b57a4474bad3bf66990839c9847683
Luca Mansi <luca@mansi.fr>
2025-03-06 13:08:19 +0100

    Refactor SQL query in 4-cities_by_state.py to include state names and improve output format

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,10 +16,9 @@ if __name__ == "__main__":
         port=3306)
     cursor = db.cursor()
     cursor.execute(
-        "SELECT cities.name FROM cities \
-            JOIN states ON cities.state_id = states.id \
-                WHERE states.name = %s ORDER BY cities.id ASC",
-        (sys.argv[4],))
+        "SELECT cities.id, cities.name, states.name FROM cities \
+        JOIN states ON cities.state_id = states.id \
+        ORDER BY cities.id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
