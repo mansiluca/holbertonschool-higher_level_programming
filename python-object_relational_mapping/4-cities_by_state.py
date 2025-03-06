90174ce380a0e07e788d0617f5eadbc7a57fcfad
Luca Mansi <luca@mansi.fr>
2025-03-06 13:06:21 +0100

    Fix SQL query to correctly join cities and states for filtering by state name

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,10 +16,11 @@ if __name__ == "__main__":
         port=3306)
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name \
-            LIKE BINARY %s ORDER BY id ASC \
-                JOIN cities ON states.id = cities.state_id",
+        "SELECT cities.name FROM cities \
+            JOIN states ON cities.state_id = states.id \
+                WHERE states.name = %s ORDER BY cities.id ASC",
         (sys.argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+
