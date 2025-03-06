f2a04da984ab17f23546f7a84c857404889be963
Luca Mansi <luca@mansi.fr>
2025-03-06 16:42:20 +0100

    Update database connection string in 7-model_state_fetch_all.py to include database name

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,8 +10,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    root_engine = create_engine('mysql+mysqldb://{}:{}@localhost'.format(
-        sys.argv[1], sys.argv[2]), pool_pre_ping=True)
+    root_engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                                format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     session = sessionmaker(bind=root_engine)()
     states = session.query(State).all()
