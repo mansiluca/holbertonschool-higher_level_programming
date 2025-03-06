804afa031b62dafc5eab8afed3e3948ecb5980be
Luca Mansi <luca@mansi.fr>
2025-03-06 16:18:06 +0100

    Add model_state.py and 6-model_state.sql for State class definition and database creation

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+python file that contains the class definition
+of a State and an instance Base = declarative_base():
+"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+meta_data = MetaData()
+Base = declarative_base(metadata=meta_data)
+
+
+
+class State(Base):
+    """
+    Class State that inherits from Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
